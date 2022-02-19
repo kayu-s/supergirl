b783507dda73cd936eb8d46b478bb2ea4333d806
yuji <yuji@BelAmer.local>
2022-02-19 23:08:54 +0900

    :+1:Appolo繋ぎ込み

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,12 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { AppRoot } from "./components/AppRoot";
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  gql,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const result = await chrome.storage.local.get("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${result["token"]}`,
+    },
+  };
+  // return the headers to the context so httpLink can read them
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export const appScript = () => {
   const body: HTMLElement = document.getElementById("root")!;
   const reactRootElement = document.createElement("div");
   body.before(reactRootElement);
-  ReactDOM.render(<AppRoot />, reactRootElement);
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <AppRoot />
+    </ApolloProvider>,
+    reactRootElement
+  );
 };
 
 appScript();
