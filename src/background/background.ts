39ac7be2b5ee1b41a6fe9cd92fe218720205cbfd
yuji <yuji@BelAmer.local>
2022-07-22 10:41:44 +0900

    :tada: コメント通知するようにした

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,59 +1,84 @@
 import { client } from "../apollo";
 import { GET_PULL_REQUESTS } from "../apollo/queries";
 import { DELAY_MINUTES, NotificationType } from "../types/options";
-import { getImageBase64, getTargetRepositories, getUuId } from "../utils";
+import {
+  getImageBase64,
+  getTargetRepositories,
+  getUuId,
+  isNotifyTarget,
+} from "../utils";
 
-const subscription = async () => {
+type CommentType = "normal" | "single" | "review";
+
+const sendCommentNotify = async (
+  node: any,
+  commentNode: any,
+  type: CommentType
+) => {
+  const { updatedAt } = commentNode;
+  if (!isNotifyTarget(updatedAt, DELAY_MINUTES)) return;
+  const { bodyText, author, url: commentUrl, pullRequest } = commentNode;
+  if (bodyText === "") return;
+  const { login, avatarUrl } = author;
+
+  const base64AvatarStr = await getImageBase64(avatarUrl);
+  await chrome.notifications.create(
+    `${commentUrl}__commentNotify__${getUuId()}`,
+    {
+      type: "basic",
+      title: `${pullRequest.title}@${node.repository.name}`,
+      message: `${login}\r\n${bodyText}`,
+      requireInteraction: true,
+      silent: false,
+      iconUrl: base64AvatarStr,
+    }
+  );
+};
+
+export const subscription = async () => {
   const repositories = await getTargetRepositories();
   client
     .query({
       query: GET_PULL_REQUESTS,
       variables: {
-        query: "is:open is:pr review-requested:@me repo:" + repositories,
+        query: "is:open is:pr repo:" + repositories,
       },
       fetchPolicy: "no-cache",
     })
     .then((result) => {
-      const nodes = result.data?.search.nodes;
+      const { viewer, search } = result.data;
+      const { nodes } = search;
+      const authorizedUser = viewer.login;
 
       // Set PR count to badge
-      const count = nodes.length;
-      if (count === 0) {
+      let count = 0;
+      for (let searchNode of nodes) {
+        for (let reviewRequestsNode of searchNode.reviewRequests.nodes) {
+          if (reviewRequestsNode.requestedReviewer.login === authorizedUser) {
+            count++;
+          }
+        }
+      }
+      if (count == 0) {
         chrome.action.setBadgeText({ text: "" });
       } else {
         chrome.action.setBadgeText({ text: String(count) });
       }
 
       // Notify comments
-      nodes.map((node: any) =>
+      nodes.map((node: any) => {
+        if (!node.reviews?.nodes) return;
+        node.reviews.nodes.filter(async (reviewsNode: any) => {
+          if (!reviewsNode) return;
+          sendCommentNotify(node, reviewsNode, "review");
+          if (!reviewsNode.comments.edges[0]?.node) return;
+          sendCommentNotify(node, reviewsNode.comments.edges[0].node, "single");
+        });
+        if (node.comments.edges.length === 0) return;
         node.comments.edges.filter(async (edge: any) => {
-          const { updatedAt } = edge.node;
-          const date = new Date(updatedAt);
-          const progressSecond = (Date.now() - date.getTime()) / 1000;
-          if (DELAY_MINUTES * 60 > progressSecond) {
-            const {
-              bodyText,
-              author,
-              url: commentUrl,
-              pullRequest,
-            } = edge.node;
-            const { login, avatarUrl } = author;
-
-            const base64AvatarStr = await getImageBase64(avatarUrl);
-            chrome.notifications.create(
-              `${commentUrl}__commentNotify__${getUuId()}`,
-              {
-                type: "basic",
-                title: `${pullRequest.title}@${node.repository.name}`,
-                message: `${login}\r\n${bodyText}`,
-                requireInteraction: true,
-                silent: false,
-                iconUrl: base64AvatarStr,
-              }
-            );
-          }
-        })
-      );
+          sendCommentNotify(node, edge.node, "normal");
+        });
+      });
     })
     .catch((e) => {
       console.log(e);
