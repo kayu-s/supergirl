7a5287184ec7fe026a4f5e0e9bfa3f1dec1ded90
michiharu <michi1diver@gmail.com>
2023-02-22 00:10:18 +0900

    :recycle: Repository と repositoryKeys の関連をより安全に定義

--- src/types/options.ts
+++ src/types/options.ts
@@ -1,8 +1,8 @@
-const repositoryKeys = ["isShow", "name"] as const;
 export type Repository = {
   isShow: boolean;
   name: string;
 };
+const repositoryKeys: (keyof Repository)[] = ["isShow", "name"];
 
 export const isRepository = (value: object): value is Repository =>
   repositoryKeys.every((key) => Object.keys(value).includes(key));
